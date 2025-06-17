3ea506a748df61d5f39114874227303ecf356b20
Abdul Basit Khalsai <abdulbasitkhalsai@gmail.com>
2025-06-17 19:20:55 +0500

    hello-chainlit completed

--- step5-chainlit/hello-chainlit/hello.py
+++ step5-chainlit/hello-chainlit/hello.py
@@ -1,6 +1,7 @@
 import chainlit as cl
 from agents import Agent, Runner, RunConfig, AsyncOpenAI, OpenAIChatCompletionsModel
 from dotenv import load_dotenv, find_dotenv
+import os
 
 load_dotenv(find_dotenv())
 
@@ -13,10 +14,37 @@ provider = AsyncOpenAI(
 
 model = OpenAIChatCompletionsModel(
     model = "gemini-2.0-flash",
-    provider = provider
+    openai_client = provider
 )
 
+run_config = RunConfig(
+    model=model,
+    model_provider = provider,
+    tracing_disabled=True
+)
+agent1 = Agent(
+    name = "Assistant",
+    instructions= "You are a helpful Assistant"
+)
+
+result = Runner.run_sync(input="Capital of Pakistan", run_config = run_config, starting_agent = agent1)
+# print(result)
+
+@cl.on_chat_start
+async def handle_chat_start():
+    cl.user_session.set("history", [])
+    await cl.Message(content="Hello! I' m the Panaversity Support Agent, How may i help you? ").send()
 
 @cl.on_message
 async def handle_message(message: cl.Message):
-    await cl.Message(content=f"Hello {message.content}").send()
+    history = cl.user_session.get("history")
+
+    history.append({"role": "user", "content":message.content})
+    result = await Runner.run(
+        agent1,
+        input=history,
+        run_config = run_config
+    )
+    history.append({"role" : "assistant", "content" : result.final_output})
+    cl.user_session.set("history", history)
+    await cl.Message(content=result.final_output).send()
